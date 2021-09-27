bd070522e8dfed59ffb77e5ee08bb3453b0a73a5
AhmetYasin07 <ahmetyasinoztop81@gmail.com>
2021-09-27 12:31:15 +0300

    pdf function

---
+++ .next/static/chunks/252f366e-9a84867da0a6ac6c97ad.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,n,u){"use strict";u.d(n,{FU5:function(){return e}});var a=u(4405);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}}]})(t)}}}]);
